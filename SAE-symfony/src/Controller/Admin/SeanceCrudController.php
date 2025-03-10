be77a225f197f3ab6a3c36aaef5204139dc03d07
Paul Espinasse <paul.espi04@gmail.com>
2025-03-10 11:31:05 +0100

    ajout de vrais themes de séances

--- SAE-symfony/src/Controller/Admin/SeanceCrudController.php
+++ SAE-symfony/src/Controller/Admin/SeanceCrudController.php
@@ -23,14 +23,24 @@ class SeanceCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            DateTimeField::new('date_heure'),
+            DateTimeField::new('date_heure')
+                ->setFormat('yyyy-MM-dd HH:mm:ss'),
             ChoiceField::new('type_seance')
                 ->setChoices([
                     'Solo' => 'solo',
                     'Duo' => 'duo',
                     'Trio' => 'trio',
                 ]),
-            TextField::new('theme_seance'),
+                ChoiceField::new('theme_seance')
+                ->setChoices([
+                    'Cardio' => 'cardio',
+                    'Renforcement musculaire' => 'renforcement musculaire',
+                    'Stretching' => 'stretching',
+                    'Pilates' => 'pilates',
+                    'Yoga' => 'yoga',
+                    'Boxe' => 'boxe',
+                    'Crossfit' => 'crossfit',
+                ]),
             ChoiceField::new('niveau_seance')
                 ->setChoices([
                     'Débutant' => 'débutant',
