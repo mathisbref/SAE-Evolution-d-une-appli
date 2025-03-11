8ffdb2855360700d4d818ea510ffb3666c8ac828
Paul Espinasse <paul.espi04@gmail.com>
2025-03-11 15:40:28 +0100

    un coach ne peut voir que lui dans la liste des coachs

--- SAE-symfony/src/Controller/Admin/CoachCrudController.php
+++ SAE-symfony/src/Controller/Admin/CoachCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Coach;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -10,6 +13,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -17,11 +21,13 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class CoachCrudController extends AbstractCrudController
 {
@@ -31,7 +37,8 @@ class CoachCrudController extends AbstractCrudController
     }
     
     public function __construct(
-        private UserPasswordHasherInterface $encoder
+        private UserPasswordHasherInterface $encoder,
+        private TokenStorageInterface $tokenStorage // 🔹 Injecter TokenStorageInterface ici
     ) {}
 
     
@@ -54,6 +61,7 @@ class CoachCrudController extends AbstractCrudController
     
     public function configureFields(string $pageName): iterable
     {
+        $user = $this->tokenStorage->getToken()?->getUser();
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('nom'),
@@ -80,10 +88,33 @@ class CoachCrudController extends AbstractCrudController
             ->renderExpanded(),
             TextField::new('fichesDePaieList', 'Fiches de paie')
             ->onlyOnIndex()
+            ->setCustomOption('mapped', false)
+            ->formatValue(fn($value, $entity) => $entity->getFichesDePaieList($user))
             ->renderAsHtml(),
             
         ];
     }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+{
+    $user = $this->tokenStorage->getToken()?->getUser();
+
+    if (!$user || !$user instanceof \App\Entity\Utilisateur) {
+        throw new \Exception("Utilisateur non trouvé ou type incorrect");
+    }
+
+    $queryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+    // Si l'utilisateur est admin, il voit tout
+    if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+        return $queryBuilder;
+    }
+
+    // Si l'utilisateur est un coach, il ne voit que son propre profil
+    return $queryBuilder
+        ->andWhere('entity.id = :coachId')
+        ->setParameter('coachId', $user->getId()); // 🔹 Remplacement de getUserIdentifier() par getId()
+}
 
     public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
     {
